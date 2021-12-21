e25d1052b64a14d020018ab6ac983cbea031ad4a
geoase <g.seyerl@posteo.at>
2021-12-21 18:19:49 +0000

    Update python modules

--- Dockerfile
+++ Dockerfile
@@ -8,10 +8,10 @@ RUN useradd --create-home appuser
 WORKDIR /home/appuser
 USER appuser
 
-COPY ["ado_era5.json","main.py", "./"]
-
 # Pull the environment name out of the environment.yml
 RUN echo "source activate $(head -1 /tmp/environment.yml | cut -d' ' -f2)" > ~/.bashrc
 ENV PATH /opt/conda/envs/$(head -1 /tmp/environment.yml | cut -d' ' -f2)/bin:$PATH
+
+COPY ["ado_era5.json","main.py", "./"]
 
 #ENTRYPOINT ["python","main.py"]
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
+import glob
 
 from cds_downloader import Downloader
 from ado_downscaler import Downscaler
 
 def main():
-    input_path = "/input"
-    output_path = "/output"
+    input_path = "/data/input"
+    output_path = "/data/output"
 
     downloader = Downloader.from_json("ado_era5.json")
-    lst_files = downloader.get_latest_daily_data("/tmp/era5", date_latency="5D")
+    lst_processes = downloader.get_latest_daily_data("/tmp/era5", date_latency="5D")
 
     filter_variable = downloader.cds_filter.get("variable")
 
@@ -16,12 +17,17 @@ def main():
         filter_variable = filter_variable.split()
 
     for var_name in filter_variable:
+        file_name = glob.glob(f"/tmp/era5/{var_name}*")[0]
+        print(file_name)
+        print(var_name)
         downscaler = Downscaler.from_filepaths(
-            os.path.join(input_path, "UERRA", var_name),
-            os.path.join(input_path, "ERA5", var_name)
+            os.path.join(input_path, "uerra", var_name + "*"),
+            os.path.join(input_path, "era5", var_name + "*")
         )
-        # ado_ds = Downscaler.from_filepaths("/mnt/ADO/ZAMG/QM/era5_uerra/daily/2m_dewpoint_temperature/uerra/*","/mnt/ADO/ZAMG/QM/era5_uerra/daily/2m_dewpoint_temperature/era5/*")
-        # ado_ds.downscale_era5("/mnt/ADO/ZAMG/ERA5/daily/2m_dewpoint_temperature_2021_4_22_reanalysis-era5-single-levels.grib","./")
+        downscaler.downscale_era5(
+            os.path.join("/tmp/era5", file_name),
+            os.path.join("./")
+        )
 
 
 if __name__ == "__main__":
