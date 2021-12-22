471c4d3c8c83ae0d6b1725cd3aa63b5dad1cd838
geoase <g.seyerl@posteo.at>
2021-12-22 21:46:37 +0000

    New paths and additional calculation of pet

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ado-pipeline
+
+Build image with tag:
+
+```
+docker build -t ado_qm_pet .
+```
+
+Run container with bind mounts and .env file (`-it` may be omitted):
+
+```
+docker run -v /mnt/ADO/ZAMG/UERRA/derived/daily:/data/input/uerra -v /mnt/ADO/ZAMG/ERA5/archive/derived:/data/input/era5 --env-file .env -it ado_pet bash
+docker run -d \
+  -it \
+  --name ado_qm_pet_test \
+  --mount type=bind,source=<INPUT DIRECTORY>,target=/home/appuser/data/input,readonly \
+  --mount type=bind,source=<OUTPUT DIRECTORY>,target=/home/appuser/data/output \
+  --env-file .env\
+  ado_qm_pet:latest
+```
+
+Copy the .env.sample file to .env and adapt the CDS API information following the 
+instructions in the [api-how-to](https://cds.climate.copernicus.eu/api-how-to).
+
+The daily archive data of "era5" and "uerra" must already be present in subdirectories within
+`<INPUT DIRECTORY>`. Be sure that the permissions are set accordingly, so that the data can be written 
+from the container.
--- main.py
+++ main.py
@@ -3,32 +3,35 @@ import glob
 
 from cds_downloader import Downloader
 from ado_downscaler import Downscaler
+from ado_downscaler import calculate_pet
 
 def main():
-    input_path = "/data/input"
-    output_path = "/data/output"
+    input_path = "~/data/input"
+    output_path = "~/data/output"
 
     downloader = Downloader.from_json("ado_era5.json")
-    lst_processes = downloader.get_latest_daily_data("/tmp/era5", date_latency="5D")
+    lst_processes = downloader.get_latest_daily_data(f"{output_path}/era5", date_latency="5D")
 
     filter_variable = downloader.cds_filter.get("variable")
 
     if not isinstance(filter_variable,list):
         filter_variable = filter_variable.split()
 
+    dct_paths = {}
     for var_name in filter_variable:
-        file_name = glob.glob(f"/tmp/era5/{var_name}*")[0]
-        print(file_name)
-        print(var_name)
+        file_name = glob.glob(f"{output_path}/era5/{var_name}*")[0]
+        dct_paths[var_name] = file_name
         downscaler = Downscaler.from_filepaths(
             os.path.join(input_path, "uerra", var_name + "*"),
             os.path.join(input_path, "era5", var_name + "*")
         )
         downscaler.downscale_era5(
-            os.path.join("/tmp/era5", file_name),
-            os.path.join("./")
+            os.path.join(f"{output_path}/era5", file_name),
+            os.path.join(f"{output_path}/qm")
         )
 
+    dct_paths["orography_reanalysis"] = f"{input_path}/uerra/orography_reanalysis-uerra-europe-single-levels.nc"}
+    calculate_pet(dct_paths)
 
 if __name__ == "__main__":
     main()
